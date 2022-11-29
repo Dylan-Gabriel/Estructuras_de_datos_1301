cf74393a130ed91e166fa952f4a2062a9d6afbd9
Dylan-Gabriel <111459132+Dylan-Gabriel@users.noreply.github.com>
2022-11-28 19:58:45 -0600

    Add files via upload

---
+++ CÓDIGO/Incluir/pqueue.h
@@ -0,0 +1,26 @@
+/*
+    pqueue.h
+*/
+#ifndef PQUEUE_H
+#define PQUEUE_H
+
+#include <heap.h>
+
+/*
+    define struct
+*/
+typedef Heap PQueue;
+
+/*
+    Public Interfaces
+*/
+#define pqueue_init heap_init
+#define pqueue_destroy heap_destroy
+#define pqueue_insert heap_insert
+#define pqueue_extract heap_extract
+
+#define pqueue_peek(pqueue) ((pqueue)->tree == NULL ? NULL : (pqueue)->tree[0])
+#define pqueue_size heap_size
+
+#endif
+    
