0080faa74ef35cbfe9705e2ce5f2a80d8d8466b7
Dylan-Gabriel <111459132+Dylan-Gabriel@users.noreply.github.com>
2022-11-28 20:04:22 -0600

    Update Actividad 2.c

--- TAREAS/programas/Arreglos/Memoria _Dinamica/Actividad 2.c
+++ TAREAS/programas/Arreglos/Memoria _Dinamica/Actividad 2.c
@@ -3,15 +3,16 @@
 
 
 int main(){
-    int nC, u, i;
-    int *p = calloc(10, sizeof(int));
+    int num;
+    int n, i;
+    int *k = calloc(10, sizeof(int));
 
-    if(p == NULL){
+    if(k == NULL){
         return -1;
     }
     
     for(i = 0; i < 10; i++){
-        p[i] = (i+1)*2;
+        k[i] = (i+1)*2;
     }
     
 	    printf("\n\n\tEsta es la sucesi%cn n%cmerica de pares:",162, 163 );
@@ -22,20 +23,20 @@ int main(){
     }
 
     printf("\n\n\tIngresa tu n%cmero de cuenta: ", 163);
-    scanf("\n\t%d", &nC);
+    scanf("\n\t%d", &num);
 
-    u = nC % 10;
+    n=num%10;
 
-    p[u] = -1;
+    k[n] = -1;
 
 /*printf("Aqu% se modifica el valor almacenado en el %cndice correspondiente al %cltimo d%gito de su n%cmero cuenta por -1: ", 161, 161, 163, 161, 163);*/
 	printf("n\n\tSe hace el recorrido de -1 con el %cltimo d%cgito de el numero de cuenta: ", 163, 161);	
     for (i = 0; i < 10; i++){
-        printf(" %d ", p[i]);
+        printf(" %d ", k[i]);
     }
 
-    p = NULL;
-    free(p);
+    k=NULL;
+    free(k);
 
     return 0;
 }
