28fa0b6c9a49e0e0b9f4659094a477db93919569
Dylan-Gabriel <111459132+Dylan-Gabriel@users.noreply.github.com>
2022-11-28 19:33:26 -0600

    Add files via upload

---
+++ CÓDIGO/Incluir/bitree.h
@@ -0,0 +1,54 @@
+/*
+    bitree.h
+*/
+#ifndef BITREE_H
+#define BITREE_H
+
+#include <stdlib.h>
+
+/*
+     Binary tree node
+*/
+typedef struct BiTreeNode_ {
+    void *data;
+    struct BiTreeNode_ *left;
+    struct BiTreeNode_ *right;
+} BiTreeNode;
+
+/*
+    Struct for binary tree nodes
+*/
+typedef struct BiTree_ {
+    int size;
+    
+    void (*destroy) (void *data);
+    
+    BiTreeNode *root;
+} BiTree;
+
+/*
+    Public Interfaces
+*/
+void bitree_init(BiTree *tree, void (*detroy)(void *data));
+void bitree_destroy(BiTree *tree);
+
+int bitree_ins_left(BiTree *tree, BiTreeNode *node, const void *data);
+int bitree_ins_right(BiTree *tree, BiTreeNode *node, const void *data);
+void bitree_rem_left(BiTree *tree, BiTreeNode *node);
+void bitree_rem_right(BiTree *tree, BiTreeNode *node);
+int bitree_merge(BiTree *merge, BiTree *left, BiTree *right, const void *data);
+
+/*
+    Macros
+*/
+#define bitree_size(tree) ((tree)->size)
+#define bitree_root(tree) ((tree)->root)
+
+#define bitree_is_eob(node) ((node) == NULL)
+#define bitree_is_leaf(node) ((node)->left == NULL && (node)->right == NULL)
+
+#define bitree_data(node) ((node)->data)
+#define bitree_right(node) ((node)->right)
+#define bitree_left(node) ((node)->left)
+
+#endif
