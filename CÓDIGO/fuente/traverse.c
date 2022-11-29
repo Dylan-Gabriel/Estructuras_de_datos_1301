a6d7a66d054d2601daa4367bf0887d0b34db1377
Dylan-Gabriel <111459132+Dylan-Gabriel@users.noreply.github.com>
2022-11-28 19:57:30 -0600

    Add files via upload

---
+++ CÓDIGO/fuente/traverse.c
@@ -0,0 +1,77 @@
+/*
+    traverse.c
+*/
+#include <stdlib.h>
+#include <string.h>
+
+#include "traverse.h"
+#include "list.h"
+
+/*
+    preorder
+*/
+int preorder(const BiTreeNode *node, List *list) {
+
+    if (!bitree_is_eob(node)) {
+
+        if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0)
+            return -1;
+
+        if (!bitree_is_eob(bitree_left(node)))
+            if (preorder(bitree_left(node), list) != 0)
+                return -1;
+
+        if (!bitree_is_eob(bitree_right(node)))
+            if (preorder(bitree_right(node), list) != 0)
+                return -1;
+    }
+
+    return 0;
+}
+
+/*
+    inorder
+*/
+
+int inorder(const BiTreeNode *node, List *list) {
+
+    if (!bitree_is_eob(node)) {
+
+        if (!bitree_is_eob(bitree_left(node)))
+            if (inorder(bitree_left(node), list) != 0)
+                return -1;
+
+        if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0)
+            return -1;
+
+        if (!bitree_is_eob(bitree_right(node)))
+            if (inorder(bitree_right(node), list) != 0)
+                return -1;
+
+    }
+
+    return 0;
+}
+
+/*
+    postorder
+*/
+int postorder(const BiTreeNode *node, List *list) {
+
+    if (!bitree_is_eob(node)) {
+
+        if (!bitree_is_eob(bitree_left(node)))
+            if (postorder(bitree_left(node), list) != 0)
+                return -1;
+
+        if (!bitree_is_eob(bitree_right(node)))
+            if (postorder(bitree_right(node), list) != 0)
+                return -1;
+
+        if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0)
+            return -1;
+    }
+
+    return 0;
+}
+
